c2b4a4e7ea21cab487c484200e651b04b3a76745
Joey-Einerhand <19751158+Joey-Einerhand@users.noreply.github.com>
2025-06-27 12:00:05 +0200

    Fixed DISCORD_USER_DATA_DIR-workaround & added icon

--- discord-multiinstance.ps1
+++ discord-multiinstance.ps1
@@ -1,61 +1,45 @@
 # How to use:
-# Pin this script to your task bar 
+# Pin this script to your task bar
 # (Drag and drop it in your task bar -> Right click -> Pin to taskbar)
 # Click on the script when you want to run a new instance of Discord.
 
 # This script assumes Discord is installed under the current windows user. 
 
-# DISKLETTER:\Users\USERNAME\AppData\Local\Discord
-$discordPath = Join-Path -Path $env:LOCALAPPDATA -ChildPath "Discord"
-
-# Whenever Discord updates, another folder is added with the following syntax:
-# app-VERSION, i.e. app-1.0.9045, app-1.0.9157, etc
-# To get Discord to run multiple times with different accounts,
-# We need to get the most recent app version, go into that folder,
-# run `Discord.exe` with the parameter `--multi-instance`
-
 function Main {
-    $latestVersionPath = Get-DiscordLatestVersionPath $discordPath
-    $discordExePath = Join-Path -Path $latestVersionPath -ChildPath "Discord.exe"
-    Start-DiscordMultiInstance $discordExePath
-}
-
-function Get-DiscordLatestVersionPath {
-    param (
-        [Parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
-        [string]$discordPath
-    )
-
-    # Array of all the app-VERSION folders, assumed to be unsorted
-    $versionFolders = Get-ChildItem -Path (Join-Path -Path $discordPath -ChildPath "app-*") -directory -Name
-
-    $latestVersionFolder = $versionFolders | Sort-Object -Descending | Select-Object -First 1
-
-    if (-not $latestVersionFolder) {
-        throw "No discord version folders found in $discordPath"
-    }
-
-    $latestVersionFolderPath = Join-Path -Path $DiscordPath -ChildPath $latestVersionFolder
-
-    return $latestVersionFolderPath 
+    Start-DiscordMultiInstance
 }
 
 function Start-DiscordMultiInstance {
-    param (
-        [Parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
-        [string]$pathToExe
-    )
+    # DISKLETTER:\Users\USERNAME\AppData\Local\Discord
+    $discordLaunchExe = Join-Path -Path $env:LOCALAPPDATA -ChildPath "Discord\Update.exe"
+    $pathToProfiles = Join-Path -Path $env:LOCALAPPDATA -ChildPath "Discord\profiles"
 
-    $exeExists = Test-Path $pathToExe
+    if (-Not (Test-Path $pathToProfiles)) {
+        New-Item -ItemType Directory -Path $pathToProfiles
+    }
 
-    if ($exeExists) {
-        Start-Process -FilePath $pathToExe -ArgumentList "--multi-instance"
+    # count running UI instances of discord
+    $discordsActive = @(Get-Process discord -ErrorAction SilentlyContinue | Where-Object { $_.MainWindowHandle -ne 0 }).Count
+    $nextDiscordIndex = $discordsActive + 1
+    # This starts profiles at either 1 or 2 depending on if the user runs one discord user the default way without this script
+    $profilePathToUse = Join-Path -Path $env:LOCALAPPDATA -ChildPath "Discord\profiles\alt$nextDiscordIndex"
+    # Clean up previous profile paths
+    while (Test-Path $profilePathToUse) {
+        # Sometimes, previously used profile paths might still be in use,
+        # even if the corresponding discord processes are closed.
+        # So, try to delete the profile path. If that's not possible, try the next index
+        try {
+            Remove-Item -Path $profilePathToUse -Recurse -Force -ErrorAction Stop 2>$null
+        }
+        catch {
+            $nextDiscordIndex += 1
+            $profilePathToUse = Join-Path -Path $env:LOCALAPPDATA -ChildPath "Discord\profiles\alt$nextDiscordIndex"
+        }
     }
-    else {
-        throw "No discord EXE found at location $pathToExe"
-    }
+    New-Item -ItemType Directory -Path $profilePathToUse
+
+    $env:DISCORD_USER_DATA_DIR = $profilePathToUse
+    Start-Process $discordLaunchExe -ArgumentList '--processStart Discord.exe --process-start-args "--multi-instance"'
 }
 
 Main
